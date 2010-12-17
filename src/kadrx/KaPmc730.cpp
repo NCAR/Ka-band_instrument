5118ce90dff5d0dab663fc30019181ac4db46300
Chris Burghart <burghart@ucar.edu>
2010-12-17 22:09:21 +0000

    new KaPmc730 class to provide singleton access to the PMC730 card on the Ka DRX machine
    git-svn-id: http://svn.eol.ucar.edu/svn/ka/trunk@104 3b647ff1-7a5c-455a-93f8-4e24338e3c8e

---
+++ src/kadrx/KaPmc730.cpp
@@ -0,0 +1,10 @@
+/*
+ * KaPmc730.cpp
+ *
+ *  Created on: Dec 17, 2010
+ *      Author: burghart
+ */
+
+#include "KaPmc730.h"
+
+Pmc730 * KaPmc730::_thePmc730 = 0;
---
+++ src/kadrx/KaPmc730.h
@@ -0,0 +1,30 @@
+/*
+ * KaPmc730.h
+ *
+ *  Created on: Dec 17, 2010
+ *      Author: burghart
+ */
+
+#ifndef KAPMC730_H_
+#define KAPMC730_H_
+
+#include "Pmc730.h"
+
+/// Control a singleton instance of Pmc730 for the one PMC730 card on the 
+/// Ka-band DRX machine. At some point, this can actually subclass Pmc730 and 
+/// gain specialized methods related to the specific connections to the PMC730 
+/// on the Ka-band radar (e.g., PMC730 DIO line 8 is connected to the PLL CLOCK+ 
+/// input on the downconverter board).
+class KaPmc730 {
+public:
+    static Pmc730 & thePmc730() {
+        if (! _thePmc730) {
+            _thePmc730 = new Pmc730(0);
+        }
+        return(*_thePmc730);
+    }
+protected:
+    static Pmc730 * _thePmc730;
+};
+
+#endif /* KAPMC730_H_ */
