c68e4713680fd9f6e1cb89186805158b6817ea88
Michael Dixon <dixon@ucar.edu>
2010-12-29 23:20:26 +0000

    Adding KaMerge
    git-svn-id: http://svn.eol.ucar.edu/svn/ka/trunk@135 3b647ff1-7a5c-455a-93f8-4e24338e3c8e

---
+++ src/kadrx/KaMerge.cpp
@@ -0,0 +1,62 @@
+#include "KaMerge.h"
+#include <logx/Logging.h>
+#include <sys/timeb.h>
+#include <cmath>
+#include <cstdlib>
+#include <unistd.h>
+#include <iostream>
+#include <iomanip>
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+using namespace boost::posix_time;
+
+LOGGING("KaMerge")
+
+///////////////////////////////////////////////////////////////////////////
+
+KaMerge::KaMerge(const KaDrxConfig& config,
+                bool publish) :
+        QThread(),
+        _config(config),
+        _publish(publish)
+{
+
+
+}
+
+/////////////////////////////////////////////////////////////////////////////
+KaMerge::~KaMerge()
+
+{
+
+}
+
+/////////////////////////////////////////////////////////////////////////////
+//
+// Thread run method
+
+void KaMerge::run()
+
+{
+
+  // Since we have no event loop, allow thread termination via the terminate()
+  // method.
+  setTerminationEnabled(true);
+  
+  // start the loop
+  while (true) {
+    usleep(100);
+  }
+}
+
+/////////////////////////////////////////////////////////////////////////////
+double KaMerge::_nowTime() {
+  struct timeb timeB;
+  ftime(&timeB);
+  return timeB.time + timeB.millitm/1000.0;
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// 1970-01-01 00:00:00 UTC
+static const ptime Epoch1970(boost::gregorian::date(1970, 1, 1), time_duration(0, 0, 0));
+
---
+++ src/kadrx/KaMerge.h
@@ -0,0 +1,65 @@
+#ifndef KA_MERGE_H_
+#define KA_MERGE_H_
+
+#include "KaDrxConfig.h"
+#include <QThread>
+
+/// KaMerge merges data from the H and V channels, and the burst channel,
+/// converts to IWRF time series format and writes the IWRF data to a client
+/// via TCP.
+///
+/// The bulk of the work done in this object is run in a separate thread.
+///
+/// The 3 instances of the KaDrxPub class each write their data to this
+/// object, inserting it into buffers (deques). The merge operation is
+/// carried out by removing items from the queues, synchronizing on the
+/// pulse numbers, and merging the data.
+///
+/// This thread acts as a server, listening for a client to connect. Only
+/// a single client is supported, given the high bandwidth of the data
+/// stream. If no client is connected, the data is discarded.
+///
+/// Since KaMerge does not use a Qt event loop at this point, the thread
+/// should be stopped by calling its terminate() method.
+
+class KaMerge : public QThread {
+
+  Q_OBJECT
+
+public:
+    
+  /**
+   * Constructor.
+   * @param config KaDrxConfig defining the desired configuration.
+   * @param publish should we publish data via DDS?
+   */
+    KaMerge(const KaDrxConfig& config,
+            bool publish);
+  
+  /// Destructor
+  virtual ~KaMerge();
+
+  /// thread run method
+  void run();
+
+private:
+
+  /// configuration
+  const KaDrxConfig& _config;
+
+  /// Return the current time in seconds since 1970/01/01 00:00:00 UTC.
+  /// Returned value has 1 ms precision.
+  /// @return the current time in seconds since 1970/01/01 00:00:00 UTC
+  double _nowTime();
+  
+  /**
+   * The number of gates being collected by the downconverter
+   */
+  unsigned int _gates;
+  
+  /// Set true if we are going to publish the data
+  bool _publish;
+
+};
+
+#endif /*KADRXPUB_H_*/
