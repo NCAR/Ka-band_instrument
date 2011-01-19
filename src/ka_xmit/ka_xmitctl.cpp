ae254289c430f9b82817a4e3c1b7f9569856c0e3
Chris Burghart <burghart@ucar.edu>
2011-01-19 18:24:05 +0000

    Now get xmitd host and port from the command line.
    git-svn-id: http://svn.eol.ucar.edu/svn/ka/trunk@223 3b647ff1-7a5c-455a-93f8-4e24338e3c8e

--- src/ka_xmit/ka_xmitctl.cpp
+++ src/ka_xmit/ka_xmitctl.cpp
@@ -4,10 +4,11 @@
  *  Created on: Jan 6, 2011
  *      Author: burghart
  */
+ 
+#include <cstdlib>
+#include <QApplication>
 
 #include <logx/Logging.h>
-
-#include <QApplication>
 
 #include "KaXmitCtlMainWindow.h"
 
@@ -19,8 +20,13 @@ main(int argc, char *argv[]) {
     logx::ParseLogArgs(argc, argv);
     
     QApplication* app = new QApplication(argc, argv);
+    
+    if (argc != 3) {
+        ELOG << "Usage: " << argv[0] << " <xmitd_host> <xmitd_port>";
+        exit(1);
+    }
 
-    QMainWindow* mainWindow = new KaXmitCtlMainWindow("localhost", 8080);
+    QMainWindow* mainWindow = new KaXmitCtlMainWindow(argv[1], atoi(argv[2]));
     mainWindow->show();
     
     return app->exec();
